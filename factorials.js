0d5755992bd9c9d0560bebd660c4ba3e3c764671
Derek Matzen <derek.matzen@gmail.com>
2017-06-15 04:31:12 -0400

    Convert to ES6

--- factorials.js
+++ factorials.js
@@ -1,12 +1,10 @@
-// Factorialize a Number
-
-function factorialize(num) {
-  var factorial = 1;
-  for (var i = 1; i < (num + 1); i++) {
+// JavaScript Factorial Calculator
+const factorialize = num => {
+  let factorial = 1;
+  
+  for (let i = 1; i < (num + 1); i++) {
     factorial *= i;
   }
-
+  
   return factorial;
 }
-
-console.log(factorialize(10));
