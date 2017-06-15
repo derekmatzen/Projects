48f2788ec0c46f6f72b9adcdf51f5d50e08e6344
Derek Matzen <derek.matzen@gmail.com>
2017-06-15 05:03:46 -0400

    Add comments

--- palindromes.js
+++ palindromes.js
@@ -1,12 +1,16 @@
 // Palindrome Checker
 const checkPalindrome = (string) => {
+    // ignore case
     let a = string.toLowerCase();
+    // regex to match special characters and whitespace
     let b = /[(\W)(\s)(_)]/g;
+    // replaces special characters and whitespace
     string = a.replace(b, '');
   
     const reverse = string.split('').reverse().join('');
     const checkPalindrome = (string === reverse);
 
+    // use ternary operator to evaluate whether `checkPalindrome` is true and provides matching result
     const result = checkPalindrome ? `${string} is a palindrome` : `${string} is not a palindrome`;
 
     return result;
